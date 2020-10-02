b9139110e0ddf1ca8abbf2499c75a8bc22ae56b5
ishiguro.yo <yoh1496@gmail.com>
2020-10-02 10:22:33 +0000

    support typescript

--- webpack.config.js
+++ webpack.config.js
@@ -17,10 +17,20 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [{ loader: 'ts-loader' }],
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
     ],
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx'],
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
   },
   output: {
     path: path.resolve(__dirname, 'build/public'),
@@ -31,6 +41,11 @@ module.exports = {
     publicPath: '/__/public/',
     historyApiFallback: {
       rewrites: [{ from: '/', to: '/dev_index.html' }],
+    },
+    host: '0.0.0.0',
+    disableHostCheck: true,
+    watchOptions: {
+      ignored: /node_modules/,
     },
   },
 };
